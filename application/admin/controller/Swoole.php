49c204067a889f7a1a7a0a7ad9ad6cf3c0bdd7f9
liujinrui <1342501157@qq.com>
2019-03-20 11:15:19 +0800

    aa

--- application/admin/controller/Swoole.php
+++ application/admin/controller/Swoole.php
@@ -5,18 +5,9 @@ class Swoole extends Base
 {
     public function index()
     {
-        return $this->fetch();
+        echo 111;
     }
 
-    public function center(){
-    	return $this->fetch();
-    }
-
-    public function ajaxmenulist(){
-    	$Menu = new \logic\menu\Menu();
-    	$list = $Menu -> get_menu_list();
-    	$menulist = nodeMerge($list['list']);
-    	return json( array( 'code'=> 1,'msg'=>'请求成功','data'=>$menulist));
-    }
+    
 
 }
