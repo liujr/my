a8cde41b39b11d3ef4e7fc2836acc4f7744cdcc3
liujinrui <1342501157@qq.com>
2019-03-20 11:29:13 +0800

    aa

--- application/admin/controller/Swoole.php
+++ application/admin/controller/Swoole.php
@@ -3,9 +3,22 @@ namespace app\admin\controller;
 
 class Swoole extends Base
 {
-    public function index()
-    {
-        echo 111;
+    private $client;
+    public function connect() {
+        if( !$this->client->connect("127.0.0.1", 9501 , 1) ) {
+            echo "Error: {$fp->errMsg}[{$fp->errCode}]\n";
+        }
+        $message = $this->client->recv();
+        echo "Get Message From Server:{$message}\n";
+
+        fwrite(STDOUT, "请输入消息：");  
+        $msg = trim(fgets(STDIN));
+        $this->client->send( $msg );
+    }
+
+    public function index(){
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+        $this->connect();
     }
 
     
