05de0e1a042486f7151252418b5d5822316749ef
liujinrui <1342501157@qq.com>
2019-04-18 14:33:12 +0800

    12

--- application/swoolw/controller/redis.php
+++ application/swoolw/controller/redis.php
@@ -1,6 +1,11 @@
 <?php
 $redisClient = new \swoole_redis;
-$redisClient->connect('127.0.0.1',6379,function(\swoole_redis,$result){
-	echo "connect".PHP_EOL;
-	var_dump($result);
+$redisClient->connect('127.0.0.1', 6379, function (\swoole_redis $client, $result) {
+    if ($result === false) {
+        echo "connect to redis server failed.\n";
+        return;
+    }
+    $client->set('key', 'swoole', function (\swoole_redis $client, $result) {
+        var_dump($result);
+    });
 });
