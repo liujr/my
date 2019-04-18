b434f366232873c52ab084e26fe49604b518e9b0
liujinrui <1342501157@qq.com>
2019-04-18 09:11:55 +0800

    12

--- application/swoolw/controller/swoole_websocket.php
+++ application/swoolw/controller/swoole_websocket.php
@@ -7,7 +7,7 @@ $server->on('open', function (\Swoole_WebSocket_Server $server, $request) {
 
 $server->on('message', function (\Swoole_WebSocket_Server $server, $frame) {
     echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-    $server->push($frame->fd, "this is server");
+    $server->push($frame->fd, "服务端返回的数据 编号为：".$frame->fd);
 });
 
 $server->on('close', function ($ser, $fd) {
