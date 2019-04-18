e326c450144b6a78c61751cc3f6384b3ed4080f1
liujinrui <1342501157@qq.com>
2019-04-18 08:59:54 +0800

    12

--- application/admin/controller/Swoole.php
+++ application/admin/controller/Swoole.php
@@ -3,22 +3,10 @@ namespace app\admin\controller;
 
 class Swoole extends Base
 {
-    private $client;
-    public function connect() {
-        if( !$this->client->connect("127.0.0.1", 9501 , 1) ) {
-            echo "Error: {$fp->errMsg}[{$fp->errCode}]\n";
-        }
-        
-        $msg = '你好';
-        $this->client->send( $msg );
+    
 
-        $message = $this->client->recv();
-        echo "Get Message From Server:{$message}\n";
-    }
-
-    public function index(){
-        $this->client = new \swoole_client(SWOOLE_SOCK_TCP);
-        $this->connect();
+    public function lists(){
+         return $this->fetch();
     }
 
     
---
+++ application/swoolw/controller/swoole_websocket.php
@@ -0,0 +1,17 @@
+<?php
+$server = new \Swoole_WebSocket_Server("0.0.0.0", 8080);
+
+$server->on('open', function (\Swoole_WebSocket_Server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";
+});
+
+$server->on('message', function (\Swoole_WebSocket_Server $server, $frame) {
+    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    $server->push($frame->fd, "this is server");
+});
+
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+
+$server->start();
