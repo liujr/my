b071d6d047370d750a43594a41886fa5e7b7d2c4
liujinrui <1342501157@qq.com>
2019-04-30 17:48:33 +0800

    ewq

--- server/http_server.php
+++ server/http_server.php
@@ -5,7 +5,7 @@ $http = new \swoole_http_server("0.0.0.0",8811);
 $http->set(
 	[
 		'enable_static_handler'  =>true,
-		'document_root' =>"/data/wwwroot/my/public/static/"
+		'document_root' =>"/data/wwwroot/my/public"
 	]
 
 );
