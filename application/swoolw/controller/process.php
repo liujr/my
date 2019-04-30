522d1e055c8f931a0f8603321047c95f474a7b5d
liujinrui <1342501157@qq.com>
2019-04-30 17:40:32 +0800

    zhipo

--- application/swoolw/controller/process.php
+++ application/swoolw/controller/process.php
@@ -1,7 +1,28 @@
 <?php
-$process = new \swoole_process(function(\swoole_process $pro){
-	echo 111;
-},false);
+$urlarr = [
+	'https://wiki.swoole.com/wiki/page/p-redis.html',
+	'https://www.baidu.com',
+	'http://www.w3school.com.cn/jsref/met_doc_write.asp',
+	'http://admin.mmrui.cn/admin/index/index',
+	'http://admin.jilisi.com/passport/login',
+	'http://store.btobvip.com/index/index'
+];
 
-$pid = $process->start();
-echo $pid.PHP_EOL;
+for($i = 0; $i<6; $i++){
+	//创建子进程
+	$process = new \swoole_process(function(\swoole_process $pro) use($i,$urlarr){
+		$res = curlData($urlarr[$i]);
+		echo $res.PHP_EOL;
+	},true);
+	$pid = $process->start();
+	$works[$pid] = $process;
+}
+foreach ($works as $key => $value) {
+	echo $value->read(); 
+}
+
+function curlData($url){
+	sleep(1);
+
+	return $url.'success'.PHP_EOL;
+}
---
+++ server/http_server.php
@@ -0,0 +1,21 @@
+<?php 
+
+$http = new \swoole_http_server("0.0.0.0",8811);
+
+$http->set(
+	[
+		'enable_static_handler'  =>true,
+		'document_root' =>"/data/wwwroot/my/public/static/"
+	]
+
+);
+
+$http->on('request',function($request,$response){
+		$response->cookie("singwa","xsssss",time()+1800);
+		$response->end("sss" . json_encode($request->get));
+});
+
+$http->start();
+
+
+ ?>
