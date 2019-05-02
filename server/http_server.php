18bdd1b81bfbb7f87d508d682e7410fa6950e6e2
liujinrui <1342501157@qq.com>
2019-05-02 10:07:34 +0800

    111

--- server/http_server.php
+++ server/http_server.php
@@ -5,11 +5,16 @@ $http = new \swoole_http_server("0.0.0.0",8811);
 $http->set(
 	[
 		'enable_static_handler'  =>true,
-		'document_root' =>"/data/wwwroot/my/public"
+		'document_root' =>"/data/wwwroot/my/public",
+        'worker_num' => 5,
 	]
-
 );
-
+$http->on('WorkerStart',function(swoole_server $server,$worker_id){
+    // 定义应用目录
+    define('APP_PATH', __DIR__ . '/../application/');
+    // 1. 加载基础文件
+    require __DIR__ . '/base.php';
+});
 $http->on('request',function($request,$response){
 		$response->cookie("singwa","xsssss",time()+1800);
 		$response->end("sss" . json_encode($request->get));
