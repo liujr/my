fc01131d579e43cda79b320d2053c378f5a9f087
liujinrui <1342501157@qq.com>
2019-05-02 11:23:29 +0800

    aa

--- server/http_server.php
+++ server/http_server.php
@@ -47,7 +47,7 @@ $http->on('request',function($request,$response) use($http){
         $res = ob_get_contents();//获取当前缓冲区内容
         ob_end_clean();// 清空（擦除）缓冲区并关闭输出缓冲
 		$response->end($res);
-        $http->close();
+        $http::close();
 });
 
 $http->start();
