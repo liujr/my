e15025bcbff3ca07a8c16c03b8e9168722efc645
liujinrui <1342501157@qq.com>
2019-05-02 11:07:26 +0800

    aa

--- server/http_server.php
+++ server/http_server.php
@@ -15,7 +15,7 @@ $http->on('WorkerStart',function(swoole_server $server,$worker_id){
     // 1. 加载基础文件
     require __DIR__ . '/../thinkphp/base.php';
 });
-$http->on('request',function($request,$response){
+$http->on('request',function($request,$response)use($http){
         //将swoole请求头信息转换为php的请求头
         if(isset($request->header)){
             foreach($request->header as $k=>$v){
@@ -23,6 +23,7 @@ $http->on('request',function($request,$response){
             }
         }
         //将swoole请get信息转换为php的get
+
         if(isset($request->get)){
             foreach($request->get as $k=>$v){
                 $_GET[$k] = $v;
@@ -37,11 +38,18 @@ $http->on('request',function($request,$response){
         ob_start(); //打开缓冲区
 
         // 2. 执行应用
-        think\App::run()->send();
+        try{
+            think\App::run()->send();
+        }catch (\Exception $e){
+
+        };
+
 
         $res = ob_get_contents();//获取当前缓冲区内容
         ob_end_clean();// 清空（擦除）缓冲区并关闭输出缓冲
 		$response->end($res);
+
+		$http->close();
 });
 
 $http->start();
